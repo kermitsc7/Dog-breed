7ce95eeaf53849aa989cef0a1d323d2349e0056c
Kermit37 <58566857+kermitsc7@users.noreply.github.com>
2023-05-25 18:22:32 +0200

    Update DownloadDrive.py

--- DownloadDrive.py
+++ DownloadDrive.py
@@ -2,7 +2,7 @@ import gdown
 
 def descarregar():
     id = "17AWuv0xkSyhW3j2bIy6ndAG89YwVhczr"
-    output = "./ResNext101_64x4d_PreTrained.pth"
+    output = "./ResNext101_32x8d_PreTrained.pth"
     gdown.download(id=id, output=output, quiet=False)
 
 
