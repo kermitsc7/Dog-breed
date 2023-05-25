51dbdae1bb18e7ad88e62575201fd3d362d2f288
Kermit37 <58566857+kermitsc7@users.noreply.github.com>
2023-05-25 18:18:57 +0200

    Update google.py

--- google.py
+++ google.py
@@ -1,6 +1,6 @@
 import gdown
 
-id = "1CCqE6FbnPmEjwVs6wWR4yqJ_WAVy-PKG"
+id = "17AWuv0xkSyhW3j2bIy6ndAG89YwVhczr"
 output = "./streamlit/ResNext101_64x4d_PreTrained.pth"
 gdown.download(id=id, output=output, quiet=False)
 
